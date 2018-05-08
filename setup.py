17aab10dabedc56c098642b7cc94f12ea679a2d7
Konstantin Baierer <unixprog@gmail.com>
2018-05-08 12:55:38 +0200

    require core@0.2.0, PAGE 2018

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     license='Apache License 2.0',
     packages=find_packages(exclude=('tests', 'docs')),
     install_requires=[
-        'ocrd >= 0.0.6',
+        'ocrd >= 0.2.0',
         'kraken',
         'click',
     ],
