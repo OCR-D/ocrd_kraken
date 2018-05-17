457ef83e87d03afc8605e91b20b60209e146f012
Konstantin Baierer <unixprog@gmail.com>
2018-05-17 17:09:29 +0200

    json_loads expects string not bytes

--- ocrd_kraken/config.py
+++ ocrd_kraken/config.py
@@ -1,5 +1,4 @@
 import json
-
 from pkg_resources import resource_string
 
-OCRD_TOOL = json.loads(resource_string(__name__, 'ocrd-tool.json'))
+OCRD_TOOL = json.loads(resource_string(__name__, 'ocrd-tool.json').decode('utf8'))
