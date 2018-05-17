4971536ec2cd7cc4d555ba3c56db314128226ee4
Konstantin Baierer <unixprog@gmail.com>
2018-05-17 16:38:39 +0200

    refactor to bundle json with tool, OCR-D/spec#30

--- ocrd_kraken/binarize.py
+++ ocrd_kraken/binarize.py
@@ -5,9 +5,15 @@ from ocrd import Processor, MIMETYPE_PAGE # pylint: disable=no-name-in-module
 from ocrd.utils import getLogger, polygon_from_points, mets_file_id
 import ocrd.model.ocrd_page as ocrd_page
 
+from ocrd_kraken.config import OCRD_TOOL
+
 log = getLogger('processor.KrakenBinarize')
 
 class KrakenBinarize(Processor):
+
+    def __init__(self, *args, **kwargs):
+        kwargs['ocrd_tool'] = OCRD_TOOL['tools'][1]
+        super(KrakenBinarize, self).__init__(*args, **kwargs)
 
     def process(self):
         """
---
+++ ocrd_kraken/config.py
@@ -0,0 +1,5 @@
+import json
+
+from pkg_resources import resource_string
+
+OCRD_TOOL = json.loads(resource_string(__name__, 'ocrd-tool.json'))
