3842999f3191afdbf9b3b4f189900a5c691161b4
Konstantin Baierer <unixprog@gmail.com>
2018-06-18 17:19:03 +0200

    v0.0.1

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     license='Apache License 2.0',
     packages=find_packages(exclude=('tests', 'docs')),
     install_requires=[
-        'ocrd >= 0.2.4',
+        'ocrd >= 0.3.0',
         'kraken',
         'click',
     ],
