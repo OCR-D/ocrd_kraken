0eecf6ce43d75ef65bd0e731745e4a9444f642ba
Robert Sachunsky <sachunsky@informatik.uni-leipzig.de>
2022-07-15 19:33:20 +0200

    Revert "segment: use binarized input only if use_legacy"
    This reverts commit b85e1478d391f6b667af7124e64c479470b0ddf8.

--- ocrd_kraken/segment.py
+++ ocrd_kraken/segment.py
@@ -71,7 +71,9 @@ class KrakenSegment(Processor):
             page = pcgts.get_Page()
             page_image, page_coords, page_info = self.workspace.image_from_page(
                 page, page_id,
-                feature_selector="binarized" if self.use_legacy else "")
+                # "binarized" if self.use_legacy else "")
+                # binarizatation seems to always be better
+                feature_selector="binarized")
             if page_info.resolution != 1:
                 dpi = page_info.resolution
                 if page_info.resolutionUnit == 'cm':
