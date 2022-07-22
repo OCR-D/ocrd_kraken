d27f1a405644ea220db417120412a8ea786ea39c
Konstantin Baierer <unixprog@gmail.com>
2022-07-22 16:12:04 +0200

    fix test_binarize

--- tests/test_binarize.py
+++ tests/test_binarize.py
@@ -2,8 +2,9 @@
 
 import os
 import shutil
+import pytest
 
-from tests.base import TestCase, assets, main
+from tests.base import assets, main
 
 from ocrd import Resolver
 from ocrd_kraken.binarize import KrakenBinarize
@@ -13,51 +14,46 @@ setOverrideLogLevel('DEBUG')
 
 PARAM_JSON = assets.url_of('param-binarize.json')
 
-WORKSPACE_DIR = '/tmp/ocrd-kraken-binarize-test'
 
-class TestKrakenBinarize(TestCase):
+@pytest.fixture()
+def workspace(tmpdir):
+    if os.path.exists(tmpdir):
+        shutil.rmtree(tmpdir)
+    workspace = Resolver().workspace_from_url(
+        assets.path_to('kant_aufklaerung_1784/data/mets.xml'),
+        dst_dir=tmpdir
+    )
+    return workspace
 
-    def setUp(self):
-        if os.path.exists(WORKSPACE_DIR):
-            shutil.rmtree(WORKSPACE_DIR)
-        os.makedirs(WORKSPACE_DIR)
 
-    #  def test_param_json(self):
-    #      resolver = Resolver()
-    #      workspace =  resolver.workspace_from_url(assets.url_of('SBB0000F29300010000/data/mets_one_file.xml'), dst_dir=WORKSPACE_DIR)
-    #      run_processor(
-    #          KrakenBinarize,
-    #          resolver=resolver,
-    #          workspace=workspace,
-    #          parameter=PARAM_JSON
-    #      )
+#  def test_param_json(self):
+#      workspace =  resolver.workspace_from_url(assets.url_of('SBB0000F29300010000/data/mets_one_file.xml'), dst_dir=WORKSPACE_DIR)
+#      run_processor(
+#          KrakenBinarize,
+#          resolver=resolver,
+#          workspace=workspace,
+#          parameter=PARAM_JSON
+#      )
 
-    def test_binarize_regions(self):
-        resolver = Resolver()
-        workspace = resolver.workspace_from_url(
-            assets.path_to('kant_aufklaerung_1784/data/mets.xml'),
-            dst_dir=WORKSPACE_DIR
-        )
-        proc = KrakenBinarize(
-            workspace,
-            input_file_grp="OCR-D-GT-PAGE",
-            output_file_grp="OCR-D-IMG-BIN-KRAKEN",
-            parameter={'level-of-operation': 'block'}
-        )
-        proc.process()
-        workspace.save_mets()
+def test_binarize_regions(workspace):
+    proc = KrakenBinarize(
+        workspace,
+        input_file_grp="OCR-D-GT-PAGE",
+        output_file_grp="OCR-D-IMG-BIN-KRAKEN",
+        parameter={'level-of-operation': 'block'}
+    )
+    proc.process()
+    workspace.save_mets()
 
-    def test_binarize_lines(self):
-        resolver = Resolver()
-        workspace = resolver.workspace_from_url(assets.url_of('kant_aufklaerung_1784/data/mets.xml'), dst_dir=WORKSPACE_DIR)
-        proc = KrakenBinarize(
-            workspace,
-            input_file_grp="OCR-D-GT-PAGE",
-            output_file_grp="OCR-D-IMG-BIN-KRAKEN",
-            parameter={'level-of-operation': 'line'}
-        )
-        proc.process()
-        workspace.save_mets()
+def test_binarize_lines(workspace):
+    proc = KrakenBinarize(
+        workspace,
+        input_file_grp="OCR-D-GT-PAGE",
+        output_file_grp="OCR-D-IMG-BIN-KRAKEN",
+        parameter={'level-of-operation': 'line'}
+    )
+    proc.process()
+    workspace.save_mets()
 
 if __name__ == "__main__":
-    main()
+    main(__file__)
