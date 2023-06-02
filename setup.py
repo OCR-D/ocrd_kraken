550d6c8b76d4f255dad456358f22e254712123b8
Robert Sachunsky <sachunsky@informatik.uni-leipzig.de>
2023-06-03 01:51:02 +0200

    setup metadata: update/improve

--- setup.py
+++ setup.py
@@ -1,32 +1,31 @@
 # -*- coding: utf-8 -*-
 """
-Installs two binaries:
+Installs the following command-line executables:
 
     - ocrd-kraken-binarize
     - ocrd-kraken-segment
+    - ocrd-kraken-recognize
 """
 import codecs
 from json import load
 
 with open('ocrd-tool.json', 'r') as f_tool:
     version = load(f_tool)['version']
-with open('requirements.txt', 'r', encoding='utf-8') as f_require:
-    install_requires = f_require.readlines()
 
 from setuptools import setup, find_packages
 
 setup(
     name='ocrd_kraken',
     version=version,
-    description='kraken bindings',
+    description='Kraken bindings',
     long_description=codecs.open('README.md', encoding='utf-8').read(),
     long_description_content_type='text/markdown',
-    author='Konstantin Baierer, Kay-Michael Würzner',
-    author_email='unixprog@gmail.com, wuerzner@gmail.com',
+    author='Konstantin Baierer, Robert Sachunsky',
+    author_email='unixprog@gmail.com, sachunsky@informatik.uni-leipzig.de',
     url='https://github.com/OCR-D/ocrd_kraken',
     license='Apache License 2.0',
     packages=find_packages(exclude=('tests', 'docs')),
-    install_requires=install_requires,
+    install_requires=open('requirements.txt').read().split('\n'),
     package_data={
         '': ['*.json', '*.yml', '*.yaml'],
     },
